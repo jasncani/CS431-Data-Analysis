8e23f05dff05c5d3215ae47acbfcc64a5701cdb9
jasncani <jasonbacani@gmail.com>
2017-12-05 18:02:49 +1000

    make regression required as sys arg

--- analyze.py
+++ analyze.py
@@ -5,26 +5,33 @@ import sys
 
 inputExtensions = ['.csv', '.dat', '.txt']
 outputExtensions = ['.png', '.jpg']
+regressionTypes = ['linear', 'quadratic']
 
-if len(sys.argv) < 2:
-    sys.exit("you must supply the input datafile and an output filename.")
+if len(sys.argv) < 4:
+    sys.exit("you must supply the input datafile, output filename, and regression type.")
 
-def getArgWithExt(extensions):
+def getArg(extensions):
     for ext in extensions:
         for arg in sys.argv:
             if ext in arg:
                 return arg
     return None
 
-inputFileName = getArgWithExt(inputExtensions)
+inputFileName = getArg(inputExtensions)
 if inputFileName == None: sys.exit('no input file detected.')
 
-outputFileName = getArgWithExt(outputExtensions)
+outputFileName = getArg(outputExtensions)
 if outputFileName == None: sys.exit('no output file detected')
+
+regressionType = getArg(regressionTypes)
+if regressionType == None: sys.exit('no regression type detected')
 
 data = fileparser.parse(inputFileName)
 
-model = reganalyzer.linearRegression(data)
+if regressionType == 'linear':
+    model = reganalyzer.linearRegression(data)
+elif regressionType == 'quadratic':
+    model = reganalyzer.quadraticRegression(data)
 
 pyplot.plot(data, 'ro', label='data')
 pyplot.plot(model, 'b', label='regression line')
