41c21b71bbc57cd79ac3ae079b9c45ad62567459
jasncani <jasonbacani@gmail.com>
2017-12-05 15:23:17 +1000

    make own main module

---
+++ analyze.py
@@ -0,0 +1,29 @@
+import fileparser
+import reganalyzer
+import plotter
+import sys
+
+inputExtensions = ['.csv', '.dat', '.txt']
+outputExtensions = ['.png', '.jpg']
+
+if len(sys.argv) < 2:
+    sys.exit("you must supply the input datafile and an output filename.")
+
+def getArgWithExt(extensions):
+    for ext in extensions:
+        for arg in sys.argv:
+            if ext in arg:
+                return arg
+    return None
+
+inputFileName = getArgWithExt(inputExtensions)
+outputFileName = getArgWithExt(outputExtensions)
+
+data = fileparser.parse(inputFileName)
+
+model = reganalyzer.linearRegression(data)
+
+plotter.plot(data, model, outputFileName)
+# plotter.drawGraph(data, 'scatter')
+# plotter.drawGraph(model, 'line')
+# plotter.displayGraph(outputFileName)
